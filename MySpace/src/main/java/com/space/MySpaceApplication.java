ef28f724a21e9ec41cda5e465a5f5bd55be8f782
lmx <3361040032@qq.com>
2022-10-16 21:06:31 +0800

    124

---
+++ MySpace/src/main/java/com/space/MySpaceApplication.java
@@ -0,0 +1,13 @@
+package com.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpaceApplication.class, args);
+    }
+
+}
---
+++ MySpace/src/test/java/com/space/MySpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
